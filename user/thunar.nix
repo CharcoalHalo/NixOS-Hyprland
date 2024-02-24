fa7751d7d70731497772291600780d9d5fc71dec
fbright <fletcbet1660@gmail.com>
2024-02-24 23:22:15 +1300

    add thuanr with plugins

---
+++ user/thunar.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  # if not using xfce needed to save prefrences
+  programs.xfconf.enable = true; 
+
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thuanr-volman
+      thunar-archive-plugin
+      thunar-media-tags-plugin
+    ];
+  };
+
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+  environment.systemPackages = with pkgs; [
+    ffmpegthumbnailer # video thumbnails
+    evince # pdf viewer
+  ];
+
+}
