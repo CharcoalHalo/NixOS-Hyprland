73c1f9f35a3d17099ba73fc27708787fc66fb0aa
fbright <fletcbet1660@gmail.com>
2024-05-06 16:30:26 +1200

    add nh

---
+++ modules/nixos/system/apps/nh.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+{
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep 5";
+    flake = "/home/fbright/.nix-config";
+  };
+
+  environment.systemPackages = with pkgs; [
+    nix-output-monitor
+    nvd
+  ];
+}
