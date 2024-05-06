09d9941685f14e50c885e67fe8d73f31ed19e802
fbright <fletcbet1660@gmail.com>
2024-05-06 17:00:10 +1200

    add sddm theme

--- modules/nixos/system/hyprland/sddm.nix
+++ modules/nixos/system/hyprland/sddm.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  services.xserver.displayManager.sddm = {
+  environment.systemPackages = with pkgs; [
+    sddm-chili-theme
+  ];
+  services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     enableHidpi = true;
+    theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
   };
 }
