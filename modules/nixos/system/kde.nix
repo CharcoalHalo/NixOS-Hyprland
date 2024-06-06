3c10871284588a63c19de3911c919ae6d77bed2e
fbright <fletcbet1660@gmail.com>
2024-06-07 02:13:10 +1200

    add kde

---
+++ modules/nixos/system/kde.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  services.desktopManager.plasma6.enable = true;
+
+  environment.plasma6.excludePackages = with pkgs.kdePacakages; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
+}
