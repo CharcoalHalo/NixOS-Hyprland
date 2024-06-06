1aebd2efcc0c37863d0bd5a14468fdef14380484
fbright <fletcbet1660@gmail.com>
2024-06-07 02:13:05 +1200

    remove darwin

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   description = "CharcoalHalo's NixOS & Hyprland Flake";
 
-  outputs = { self, nixpkgs, home-manager, nix-darwin, nix-homebrew, 
-              homebrew-bundle, homebrew-cask, homebrew-core, ... } @inputs:
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
 
   let
     userSettings = rec {
@@ -43,62 +42,23 @@
         };
       };
     };
-
-    darwinConfigurations."Fletchers-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [
-        ./hosts/darwin/configuration.nix
-        home-manager.darwinModules.home-manager
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install under default prefix and install for Rosetta
-            enable = true;
-            enableRosetta = false;
-
-            # User owning the Homebrew prefix
-            user = "${userSettings.username}";
-
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
-            };
-            mutableTaps = false;
-            # Automatically migrate existing Homebrew installs
-            # autoMigrate = false;
-          };
-        }
-      ];
-    };
   };
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+      nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      home-manager.url = "github:nix-community/home-manager/master";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # NixOS
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      inputs.hyprland.follows = "hyprland";
-    };
-    hyprsplit.url = "github:shezdy/hyprsplit";
-    hyprsplit.inputs.hyprland.follows = "hyprland";
+      # NixOS
+      hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      split-monitor-workspaces = {
+        url = "github:Duckonaut/split-monitor-workspaces";
+        inputs.hyprland.follows = "hyprland";
+      };
+      hyprsplit.url = "github:shezdy/hyprsplit";
+      hyprsplit.inputs.hyprland.follows = "hyprland";
 
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
-
-    # Macos
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    homebrew-bundle.url = "github:homebrew/homebrew-bundle";
-    homebrew-bundle.flake = false;
-    homebrew-core.url = "github:homebrew/homebrew-core";
-    homebrew-core.flake = false;
-    homebrew-cask.url = "github:homebrew/homebrew-cask";
-    homebrew-cask.flake = false;
+      spicetify-nix.url = "github:the-argus/spicetify-nix";
   };
 }
