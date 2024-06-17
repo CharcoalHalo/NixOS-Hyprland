1ba30b7dd7074517e4e9e0e5cb9fdf7145647d15
fbright <fletcbet1660@gmail.com>
2024-06-17 20:32:43 +1200

    catppuccin sddm theme on qt6

--- modules/nixos/system/hyprland/sddm-theme.nix
+++ modules/nixos/system/hyprland/sddm-theme.nix
@@ -1,12 +1,28 @@
 { pkgs }:
 
-pkgs.stdenv.mkDerivation {
+# sugar dark for qt5
+/* pkgs.stdenv.mkDerivation {
   name = "sugar-dark";
   src = pkgs.fetchFromGitHub {
     owner = "MarianArlt";
     repo = "sddm-sugar-dark";
     rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
     sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+  };
+  installPhase = ''
+    mkdir -p $out
+    cp -R ./* $out/
+  '';
+} */
+
+# Puppy's SDDM Theme qt6
+pkgs.stdenv.mkDerivation {
+  name = "SDDM-Theme";
+  src = pkgs.fetchFromGitHub {
+    owner = "PuppyAnimations";
+    repo = "SDDM-Theme";
+    rev = "b9f30c62d8a1f0eba3d67e34fb0bb7c2ae3234d2";
+    sha256 = "15hzmn6manrlg03adgy0mgs1wf85i0dc9pbp7gsjkzm6cinlwm97";
   };
   installPhase = ''
     mkdir -p $out
--- modules/nixos/system/hyprland/sddm.nix
+++ modules/nixos/system/hyprland/sddm.nix
@@ -1,13 +1,46 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtgraphicaleffects
-  ];
+  #environment.systemPackages = with pkgs; [
+    #libsForQt5.qt5.qtquickcontrols2
+    #libsForQt5.qt5.qtquickcontrols
+    #libsForQt5.qt5.qtgraphicaleffects
+   # libsForQt5.qt5.qtsvg
+    #libsForQt5.qt5.qtbase
+    
+    #sddm-chili-theme
+  #];
+  environment.systemPackages = [(
+  pkgs.catppuccin-sddm.override {
+    flavor = "mocha";
+    font  = "Noto Sans";
+    fontSize = "9";
+    #background = "${./wallpaper.png}";
+    loginBackground = false;
+  }
+)];
+
   services.displayManager.sddm = {
     enable = true;
+    #package = pkgs.lib.mkForce pkgs.libsForQt5.sddm;
+    #extraPackages = pkgs.lib.mkForce [ pkgs.libsForQt5.qt5.qtgraphicaleffects ];
+    extraPackages = with pkgs; [     
+      kdePackages.qt6ct
+      kdePackages.qt5compat
+      kdePackages.qtdeclarative
+      kdePackages.qtsvg
+    ];
     wayland.enable = true;
     enableHidpi = true;
-    theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
+    theme = "catppuccin-mocha";#"${import ./sddm-theme.nix { inherit pkgs; }}";
+
+    settings = {
+      General = {
+        DefaultSession = "hyprland.desktop";
+      };
+      Theme = {
+        CursorTheme = "Bibata-Modern-Ice";
+        CursorSize = "22";
+      };
+    };
   };
 }
