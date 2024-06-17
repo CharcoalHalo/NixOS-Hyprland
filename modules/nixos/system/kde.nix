3593944d98afc06d5f9fabfea41c621329e182eb
fbright <fletcbet1660@gmail.com>
2024-06-17 20:31:42 +1200

    spelling mistake kde

--- modules/nixos/system/kde.nix
+++ modules/nixos/system/kde.nix
@@ -1,8 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.desktopManager.plasma6.enable = true;
 
-  environment.plasma6.excludePackages = with pkgs.kdePacakages; [
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
     konsole
     oxygen
