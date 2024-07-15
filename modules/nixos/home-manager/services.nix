e97ae8cbfa2c79d749ec742ff33d29be41772835
fbright <fletcbet1660@gmail.com>
2024-07-15 23:49:18 +1200

    enable cliphist service

--- modules/nixos/home-manager/services.nix
+++ modules/nixos/home-manager/services.nix
@@ -4,4 +4,9 @@
   services.ssh-agent.enable = true;
   
   services.easyeffects.enable = true; #requires programs.dconf.enable = true; (enabled in hyprland for now)
+
+  services.cliphist = {
+    enable = true;
+    allowImages = true;
+  };
 }
