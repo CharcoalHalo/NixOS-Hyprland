95a4c218fde70bc30cf9408094152bb30fc2e702
fbright <fletcbet1660@gmail.com>
2024-07-15 23:51:58 +1200

    add extra modules

--- hosts/virtues/home-manager.nix
+++ hosts/virtues/home-manager.nix
@@ -10,12 +10,17 @@
     ../../modules/nixos/home-manager/apps/librewolf.nix
     ../../modules/nixos/home-manager/apps/spicetify.nix
     ../../modules/nixos/home-manager/apps/neovim.nix
+    ../../modules/nixos/home-manager/apps/development.nix
+    ../../modules/nixos/home-manager/apps/gaming.nix
+
     ../../modules/nixos/home-manager/hyprland/hyprconf.nix
     ../../modules/nixos/home-manager/hyprland/hypridle+lock.nix
     ../../modules/nixos/home-manager/hyprland/rofi.nix
     ../../modules/nixos/home-manager/hyprland/theme.nix
-    ../../modules/nixos/home-manager/apps/development.nix
-    ../../modules/nixos/home-manager/apps/gaming.nix
+    ../../modules/nixos/home-manager/hyprland/swaync.nix
+    ../../modules/nixos/home-manager/hyprland/waybar.nix
+    ../../modules/nixos/home-manager/hyprland/wlogout.nix
+
     ../../modules/nixos/home-manager/services.nix
     ../../modules/nixos/home-manager/shell.nix
     ../../modules/nixos/home-manager/xdg.nix
